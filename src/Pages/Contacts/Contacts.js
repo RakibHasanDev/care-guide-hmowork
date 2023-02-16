f18bc8cbe8f2d505cba4f291ca909e0ef648b3b4
“RakibHasanDev” <rakibkst16@gmail.com>
2023-02-16 13:57:57 -0800

    added another section

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -6,7 +6,7 @@ const Contacts = () => {
         <div className='min-h-screen -mt-20 '>
             <div className=' bg-contact '>
                 <div className='h-full w-full bg-simple py-3 '>
-                    <div className=' lg:w-[50%]  mx-auto mt-[25vh] lg:text-4xl text-xl text-center text-white font-semibold openSans'>
+                    <div className=' lg:w-[50%]  mx-auto mt-[25vh] lg:text-4xl text-xl text-center text-white openSans lobStar'>
                         <p className='mt-2'>
 
                             Welcome Cottage Home Care Service
